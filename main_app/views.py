23eb9fb2d4d0514c61e9787eeaa5ad40abeb79f5
gentlevulture <novemberboughs@gmail.com>
2021-11-29 04:03:35 -0800

    addSingupURL&Function

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from .models import Figure
 
@@ -34,3 +36,17 @@ class FigureUpdate(UpdateView):
 class FigureDelete(DeleteView):
   model = Figure
   success_url = '/figures/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('figures_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
