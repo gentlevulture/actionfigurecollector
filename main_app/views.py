8beb0312467514425c50c0c6ecc974a712cb9e57
gentlevulture <novemberboughs@gmail.com>
2021-11-28 23:54:55 -0800

    renderFigureData

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Figure:
+  def __init__(self, name, brand, description, scale):
+    self.name = name
+    self.brand = brand
+    self.description = description
+    self.scale = scale
+
+figures = [
+  Figure('Medieval Spawn III', 'McFarlane', 'Missing shield.', 6),
+  Figure('Grand Admiral Thrawn', 'Hasbro', 'On card.', 6),
+  Figure('Boba Fett', 'Hasbro', 'On card.', 3.75),
+  Figure('Batman - DC Artists Alley', 'DC Entertainment', 'Designed by nooligan. Loose.', 6)
+]
+
+def figures_index(request):
+  return render(request, 'figures/index.html', { 'figures': figures })
