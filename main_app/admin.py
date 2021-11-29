0c6f6191f280c4b487808b5192c6efdbc293f49f
gentlevulture <novemberboughs@gmail.com>
2021-11-29 11:19:55 -0800

    photoAdmin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Figure, Comic
+from .models import Figure, Comic, Photo
 
 admin.site.register(Figure)
 admin.site.register(Comic)
+admin.site.register(Photo)
